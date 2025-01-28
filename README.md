5863df75e3478d8fd8f4526119d8a37d600ac9eb
Cedar <songcedar@outlook.com>
2025-01-28 22:26:51 +0800

    Add json library, automate version info with CMake, enhance logging with spdlog, implement LibFile class

--- README.md
+++ README.md
@@ -6,20 +6,31 @@ command line tool to validate standard cell libraries in `.lib` format.
 
 ## Usage
 
-```bash
+```zsh
 ZlibValidation
 Usage: ./zlibvalidation [OPTIONS]
 
 Options:
-  -h,--help                   Print this help message and exit
-  -v,--version                Show version information
-  --file TEXT:FILE            Specify the file to process
+  -h,--help                                  Print this help message and exit
+  -v,--version                               Display program version information and exit
+  -m,--mode TEXT:{parse,modify} [parse]      Choose the mode: 'parse' or 'modify'
+  -f,--file TEXT:FILE REQUIRED               Specify the file to process
 ```
 
 ## Development Diary
 
 ### 2025-01-27
 
-- Create blank project, use external `libsi2dr_liberty.a` library
-- Add command line argument parsing using `CLI11` library
-- Add `spdlog` logging library
+- 创建空项目，手动添加使用外部 `libsi2dr_liberty.a` 库
+- 添加 `CLI11` 库处理命令行参数解析
+- 添加 `spdlog` 日志库，高效日志格式化输出
+
+### 2025-01-28
+
+- 添加 `nlohmann/json` 库用于 JSON 数据存储和检索
+- 在 `version.h` 中存储项目作者、版本、构建日期等信息，由 `CMake` 自动维护
+- 简化了 `--version` 参数解析的代码实现，并添加了 `--mode` 选项以选择工作模式
+- 使用 `spdlog` 库的日志记录器将 debug 级别及以上的日志输出到文件，将 info 级别及以上的日志输出到控制台
+- 实现了 `LibFile` 类，用于封装读取、解析和修改库文件的功能，包含名称和 PVT 信息作为公共属性
+  - 但仍然使用过程化方法读取和输出库文件名称和 PVT 信息
+- 按照 C++ 标准库、第三方库和本地库的顺序重新排列了 `#include` 语句
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,198 @@
-#include "CLI/CLI.hpp"
-#include "spdlog/spdlog.h"
-#include "si2dr_liberty.h"
+// ./src/main.cpp
 #include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <string>
 
-void print_version() {
-  spdlog::info("ZlibValidation version 0.1.0");
-  spdlog::info("Author: Song Zixuan, 2025-01-26");
+#include "CLI/CLI.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "si2dr_liberty.h"
+#include "nlohmann/json.hpp"
+
+#include "version.h"
+
+void print_info() {
+  spdlog::info("Version {}, Built: {}", APP_VERSION, BUILD_TIMESTAMP);
+  spdlog::info("Author: {}, Email: {}", APP_AUTHOR, APP_CONTACT);
+}
+
+// Forward declarations
+class LibAttribute;
+class LibGroup;
+class LibFile;
+
+
+
+class LibFile {
+public:
+  std::string libname_;
+  int process_;
+  float voltage_;
+  int temperature_;
+
+  LibFile(const std::string &filename) : filename_(filename){
+    si2drPIInit(&err_);
+  }
+
+  ~LibFile() {
+    spdlog::info("Closing '{}' ...", filename_);
+    si2drPIQuit(&err_);
+  }
+
+  void read() {
+    spdlog::info("Reading '{}' ...", filename_);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    si2drReadLibertyFile(const_cast<char *>(filename_.c_str()), &err_);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    if (err_ == SI2DR_INVALID_NAME) {
+      spdlog::error("COULD NOT OPEN {} for parsing-- quitting...", filename_);
+      exit(301);
+    } else if (err_ == SI2DR_SYNTAX_ERROR) {
+      spdlog::error("Syntax Errors were detected in the input file!");
+      exit(401);
+    } else {
+      spdlog::info("Done. Read time: {:.2f} seconds", duration.count());
+    }
+  }
+
+  void parse() {
+    spdlog::info("Parsing the file...");
+
+    si2drGroupsIdT groups;
+    si2drGroupIdT group;
+    groups = si2drPIGetGroups(&err_);
+    while (!si2drObjectIsNull(group = si2drIterNextGroup(groups, &err_), &err_)) {
+      si2drNamesIdT gnames;
+      si2drStringT gname;
+
+      gnames = si2drGroupGetNames(group, &err_);
+      gname = si2drIterNextName(gnames, &err_);
+      si2drIterQuit(gnames, &err_);
+
+      if (gname) {
+        libname_ = gname;
+        spdlog::info("Library Name: {}", libname_);
+      } else {
+        spdlog::warn("Library Name: <NONAME>");
+      }
+
+      si2drGroupsIdT sub_groups;
+      si2drGroupIdT sub_group;
+      sub_groups = si2drGroupGetGroups(group, &err_);
+      while (!si2drObjectIsNull(sub_group = si2drIterNextGroup(sub_groups, &err_), &err_)) {
+        si2drStringT sub_group_type = si2drGroupGetGroupType(sub_group, &err_);
+        si2drNamesIdT sub_group_names = si2drGroupGetNames(sub_group, &err_);
+        si2drStringT sub_group_name = si2drIterNextName(sub_group_names, &err_);
+
+        si2drIterQuit(sub_group_names, &err_);
+        if (!strcmp(sub_group_type, "cell")) {
+          spdlog::debug("Cell Name: {}", sub_group_name);
+        }	else if (!strcmp(sub_group_type, "operating_conditions"))
+        {
+          spdlog::info("Operating Conditions: {}", sub_group_name);
+          // print all the attrs
+          si2drAttrsIdT attrs;
+          si2drAttrIdT attr;
+          attrs = si2drGroupGetAttrs(sub_group, &err_);
+          for (attr = si2drIterNextAttr(attrs, &err_);
+              !si2drObjectIsNull(attr, &err_);
+              attr = si2drIterNextAttr(attrs, &err_))
+          {
+            si2drAttrTypeT at = si2drAttrGetAttrType(attr, &err_);
+            si2drValuesIdT  vals;
+            si2drInt32T     intgr= si2drSimpleAttrGetInt32Value(attr,&err_);
+            si2drFloat64T   float64= si2drSimpleAttrGetFloat64Value(attr,&err_);
+            si2drStringT    string, nam= si2drAttrGetName(attr,&err_);
+            if (!strcmp(nam, "process"))
+              process_ = intgr;
+            if (!strcmp(nam, "temperature"))
+              temperature_ = intgr;
+            if (!strcmp(nam, "voltage"))
+              voltage_ = float64;
+          }
+          si2drIterQuit(attrs, &err_);
+          spdlog::info("Process: {}, Temperature: {}, Voltage: {}", process_, temperature_, voltage_);
+          continue;
+        }      
+      }
+      si2drIterQuit(sub_groups, &err_);
+    }
+    si2drIterQuit(groups, &err_);
+
+  }
+
+  void modify() {
+    spdlog::info("Modifying the file...");
+  }
+
+private:
+  std::string filename_;
+  si2drErrorT err_;
+};
+
+
+
+/**
+ * @brief Sets up the logger with both console and file sinks.
+ *
+ * This function initializes a logger with two sinks: one for console output
+ * with color and another for file output. The console sink logs messages at
+ * the info level and above, while the file sink logs messages at the debug
+ * level and above. The logger itself is set to log messages at the debug level
+ * and above. The logger is then set as the default logger for the application.
+ */
+void setup_logger() {
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  console_sink->set_level(spdlog::level::info);
+
+  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logfile.log", true);
+  file_sink->set_level(spdlog::level::debug);
+
+  std::vector<spdlog::sink_ptr> sinks {console_sink, file_sink};
+  auto logger = std::make_shared<spdlog::logger>(APP_NAME, sinks.begin(), sinks.end());
+  logger->set_level(spdlog::level::debug);
+  spdlog::set_default_logger(logger);
 }
 
 int main(int argc, char *argv[]) {
-  CLI::App app{"ZlibValidation"};
+  // Setup the global logger
+  setup_logger();
 
-  bool show_version = false;
+  // Parse command line arguments
   std::string filename;
+  std::string mode;
 
-  app.add_flag("-v,--version", show_version, "Show version information");
-  app.add_option("--file", filename, "Specify the file to process")
-      ->check(CLI::ExistingFile);
-
+  // Command line parameter parsing using CLI11
+  CLI::App app{APP_NAME};
+  app.set_version_flag("-v,--version", APP_VERSION);
+  app.add_option("-m,--mode", mode, "Choose the mode: 'parse' or 'modify'")
+      ->default_val("parse")
+      ->check(CLI::IsMember({"parse", "modify"}));
+  app.add_option("-f,--file", filename, "Specify the file to process")
+      ->check(CLI::ExistingFile)
+      ->required();
+  auto formatter = std::make_shared<CLI::Formatter>();
+  formatter->column_width(45);
+  app.formatter(formatter);
   CLI11_PARSE(app, argc, argv);
 
-  if (show_version) {
-    print_version();
-    return 0;
-  }
+  print_info();
 
   // Begin reading the file
-  print_version();
-  spdlog::info("Reading '{}' ...", filename);
+  LibFile libfile(filename);
+  libfile.read();
 
-  si2drErrorT err;
-  si2drPIInit(&err);
-
-  auto start = std::chrono::high_resolution_clock::now();
-  si2drReadLibertyFile(const_cast<char *>(filename.c_str()), &err);
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> duration = end - start;
-
-  if (err == SI2DR_INVALID_NAME) {
-    spdlog::error("COULD NOT OPEN {} for parsing-- quitting...", filename);
-    return 301;
-  } else if (err == SI2DR_SYNTAX_ERROR) {
-    spdlog::error("Syntax Errors were detected in the input file!");
-    return 401;
-  } else {
-    spdlog::info("Done. Read time: {:.2f} seconds", duration.count());
+  if (mode == "parse") {
+    libfile.parse();
+  } else if (mode == "modify") {
+    libfile.modify();
   }
 
-  si2drGroupsIdT groups;
-  si2drGroupIdT group;
-  groups = si2drPIGetGroups(&err);
-  while (!si2drObjectIsNull(group = si2drIterNextGroup(groups, &err), &err)) {
-    si2drNamesIdT gnames;
-    si2drStringT gname;
-
-    gnames = si2drGroupGetNames(group, &err);
-    gname = si2drIterNextName(gnames, &err);
-    si2drIterQuit(gnames, &err);
-
-    if (gname) {
-      spdlog::info("Library Name: {}", gname);
-    } else {
-      spdlog::error("Library Name: <NONAME>");
-      return 1;
-    }
-  }
-  si2drIterQuit(groups, &err);
-
-  spdlog::info("Quitting...");
-  si2drPIQuit(&err);
-  spdlog::info("Done!");
+  spdlog::info("All Done!");
   return 0;
 }
