3203729feffacb31355c09ca95b6140217cef989
Cedar <songcedar@outlook.com>
2025-04-14 23:52:50 +0800

    refactor: reorganize test script for clarity and structure

--- test/test.sh
+++ test/test.sh
@@ -11,10 +11,18 @@ ninja
 ./zlibvalidation --version
 
 ## mono sub command
+
 cd ~/Projects/ZlibValidation/build
 ./zlibvalidation clear
 ./zlibvalidation -h
 ./zlibvalidation mono ~/examples/liberate_lv/LIBRARY/example.lib -s
+
+## supercell sub command
+
+cd ~/Projects/ZlibValidation/build
+./zlibvalidation clear
+./zlibvalidation -h
+./zlibvalidation supercell ~/examples/liberate_lv/LIBRARY/example.lib -c 3
 
 ## verilog sub command
 
@@ -29,3 +37,17 @@ cd ~/Projects/ZlibValidation/build
 ./zlibvalidation clear
 ./zlibvalidation -h
 ./zlibvalidation spice ../pdk/tcbn65lpbc.lib -c 3 --cells FA1D0 INVD0
+
+## compare sub command
+
+cd ~/Projects/ZlibValidation/build
+./zlibvalidation clear
+./zlibvalidation -h
+./zlibvalidation compare --ref ../pdk/tcbn65lpbc.ski.lib --comp ../pdk/tcbn65lpbc.nldm.pt.lib
+
+## func sub command
+
+cd ~/Projects/ZlibValidation/build
+./zlibvalidation clear
+./zlibvalidation -h
+./zlibvalidation func --ref ../pdk/tcbn65lpbc.lib --comp ../pdk/tcbn65lp.v --cells IAO21D0 CMPE42D1
